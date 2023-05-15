a10eb77ac9a7fe4c71f8a70bcdc37ab9c46da796
S.K Shuvo <zamiulbasedshuvo444@gmail.com>
2023-05-15 14:30:16 +0600

    recipe added

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors())
 
 
 const chefs = require('./data/chef.json')
+// console.log(chefs);
+
 
 app.get('/', (req, res) => {
   res.send('server is running...')
+})
+app.get('/allData/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log(id);
+  const singleData = chefs.find(p => p.id === id);
+  
+res.send(singleData)
 })
 
 app.get('/allData', (req, res) => {
